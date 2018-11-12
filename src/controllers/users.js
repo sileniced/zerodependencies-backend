aa9ef971fe27a6a4788d5ed0e963e66519123736
Vince Liem <vincepaul.liem@gmail.com>
2018-11-12 12:09:02 +0100

    Entity Manager and such

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,20 +1,28 @@
-const entity = require('../../lib/entityCreator')
-const validator = require('../../lib/validator')
+const em = require('../../lib/entityManager')
 const crud = require('../../lib/jsonDriver')
 
-const { hash, compare } = require('../../lib/passwordHasher')
+const User = ['firstName', 'lastName', { identifier: 'email' }, { password: 'password' }, { boolean: 'tos' }]
 
 class users {
 
+  constructor() {
+    this.name = 'users'
+  }
+
   GET(id) {
     return async (data, callback) => {
-      const err = []
-      const errCatcher = err => {
-        err.push(err)
-      }
-      if (!id) {
 
+      const errs = { 400: [], 500: [] }
+      const errCatcher = err => {
+        if (typeof(err) === 'string') errs[400].push(err)
+        else errs[500].push(err)
       }
+
+      const user = await crud.read(this.name, id, errCatcher)
+
+      if (errs[500].length) callback(500, { errs })
+      else if (errs[400].length) callback(400, { errs })
+      else callback(200, { user })
     }
   }
 
@@ -27,20 +35,14 @@ class users {
         else errs[500].push(err)
       }
 
-      const User = ['firstName', 'lastName', 'email', 'password', { boolean: 'tos' }]
-      const valid = validator(User, data.payload, errCatcher)
+      const user = await em.create(this.name, User, data.payload, errCatcher)
 
-      if (!await crud.isUnique('users', valid.email)) errCatcher('user already exists')
       if (!errs[400].length) {
-
-        const user = await entity.create('users', valid, errCatcher)
-        user.password = hash(user.password, errCatcher)
-        await crud.create('users', user.id, user, errCatcher)
+        await em.commit(this.name, User, user, errCatcher)
 
         if (errs[500].length) callback(500, { errs })
         else callback(201, { user })
       } else callback(400, { errs })
-
     }
   }
 
